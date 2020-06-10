d34f3e96ff8d741d31b0bd9af3e0c40f276ae99d
renat <renatuvarov1311@gmail.com>
2020-06-10 09:52:37 +0300

    recaptcha guzzle

--- app/Http/Middleware/RecaptchaMiddleware.php
+++ app/Http/Middleware/RecaptchaMiddleware.php
@@ -3,40 +3,48 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
 
 class RecaptchaMiddleware
 {
     /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
+     * @var Client
+     */
+    private $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @param Request $request
+     * @param Closure $next
+     * @return \Illuminate\Http\RedirectResponse|mixed
      */
     public function handle($request, Closure $next)
     {
-        if ( ! env('APP_DEBUG')) {
-            $results = $this->getCaptcha($request->input('g-recaptcha-response'));
+        $results = $this->getCaptcha($request->input('g-recaptcha-response'), $request->ip());
 
-            if ($results->success && $results->score > 0.5) {
-                return $next($request);
-            }
-
-            return back()->with('error', 'error');
+        if ($results->success && $results->score > 0.5) {
+            return $next($request);
         }
 
-        return $next($request);
+        return back()->with('error', 'error');
     }
 
-    private function getCaptcha(string $secretKey = '')
+    private function getCaptcha(?string $recaptchaResponse = '', ?string $ip = '')
     {
-        $results = file_get_contents(
-            'https://www.google.com/recaptcha/api/siteverify?secret='
-            . env('SECRET_KEY')
-            . '&response='
-            . $secretKey
-        );
+        $results = $this->client->post(env('RECAPTCHA_URL'), [
+            'form_params' => [
+                'secret' => env('SECRET_KEY'),
+                'response' => $recaptchaResponse,
+                'remoteip' => $ip,
+            ],
+            'verify' => ! env('APP_DEBUG'),
+        ]);
 
-        return json_decode($results);
+        return json_decode($results->getBody());
     }
 }
--- resources/views/parts/recaptcha.blade.php
+++ resources/views/parts/recaptcha.blade.php
@@ -1,12 +1,10 @@
-@if( ! env('APP_DEBUG'))
-    @push('js')
-        <script src="https://www.google.com/recaptcha/api.js?render={{ env('SITE_KEY') }}"></script>
-        <script>
-            grecaptcha.ready(function () {
-                grecaptcha.execute('{{ env('SITE_KEY') }}', {action: 'homepage'}).then(function (token) {
-                    document.querySelector('#g-recaptcha-response').value = token;
-                });
+@push('js')
+    <script src="https://www.google.com/recaptcha/api.js?render={{ env('SITE_KEY') }}"></script>
+    <script>
+        grecaptcha.ready(function () {
+            grecaptcha.execute('{{ env('SITE_KEY') }}', {action: 'homepage'}).then(function (token) {
+                document.querySelector('#g-recaptcha-response').value = token;
             });
-        </script>
-    @endpush
-@endif
+        });
+    </script>
+@endpush
